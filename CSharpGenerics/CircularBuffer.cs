a59d51a1e1c74ac326bf5eb5e3d16b15a14ef9a4
Kevin J Donohue <kevinjdonohue@yahoo.com>
2016-12-29 23:06:16 -0800

    +added circular buffer console app +refactored circular buffer class to be generic +added xunit runner, fluent assertions, and auto fixture nuget packages to unit test project +renamed circular buffer test class

--- CSharpGenerics/CircularBuffer.cs
+++ CSharpGenerics/CircularBuffer.cs
@@ -1,7 +1,45 @@
-﻿namespace CSharpGenerics
+﻿using System;
+
+namespace CSharpGenerics
 {
-    public class CircularBuffer
+    public class CircularBuffer<T>
     {
-        
+        private readonly T[] _buffer;
+        private int _start;
+        private int _end;
+
+        public CircularBuffer() : this(10)
+        {
+        }
+
+        public CircularBuffer(int capacity)
+        {
+            _buffer = new T[capacity + 1];
+            _start = 0;
+            _end = 0;
+        }
+
+        public int Capacity => _buffer.Length;
+
+        public bool IsEmpty => _end == _start;
+
+        public bool IsFull => (_end + 1) % _buffer.Length == _start;
+
+        public void Write(T value)
+        {
+            _buffer[_end] = value;
+            _end = (_end + 1) % _buffer.Length;
+            if (_end == _start)
+            {
+                _start = (_start + 1) % _buffer.Length;
+            }
+        }
+
+        public T Read()
+        {
+            T result = _buffer[_start];
+            _start = (_start + 1) % _buffer.Length;
+            return result;       
+        }    
     }
 }
