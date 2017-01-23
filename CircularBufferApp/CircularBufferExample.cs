eb9a4e77df7549e004b8029e2b8e1969bc4e8926
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-01-22 18:51:41 -0800

    Refactored the CircularBuffer logic out of the main console program so that we an use it for other examples

---
+++ CircularBufferApp/CircularBufferExample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CSharpGenerics;
+
+namespace CircularBufferApp
+{
+    public class CircularBufferExample
+    {
+        public void Run()
+        {
+            IBuffer<double> buffer = InstantiateCircularBuffer();
+
+            ProcessUserInput(buffer);
+
+            ProcessBuffer(buffer);
+        }
+
+        private IBuffer<double> InstantiateCircularBuffer()
+        {
+            BufferFactory bufferFactory = new BufferFactory();
+            IBuffer<double> buffer = bufferFactory.GetInstance<double>(BufferType.CircularBuffer);
+
+            return buffer;
+        }
+
+        private void ProcessBuffer(IBuffer<double> buffer)
+        {
+            double sum = 0.0;
+
+            Console.WriteLine("Buffer: ");
+
+            while (!buffer.IsEmpty)
+            {
+                sum += buffer.Read();
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private void ProcessUserInput(IBuffer<double> buffer)
+        {
+            while (true)
+            {
+                double value = 0.0;
+
+                string input = Console.ReadLine();
+
+                if (Double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
--- CircularBufferApp/Program.cs
+++ CircularBufferApp/Program.cs
@@ -7,46 +7,9 @@ namespace CircularBufferApp
     {
         static void Main(string[] args)
         {
-            BufferFactory bufferFactory = new BufferFactory();
-            IBuffer<double> buffer = bufferFactory.GetInstance<double>(BufferType.CircularBuffer);
-
-            ProcessUserInput(buffer);
-
-            ProcessBuffer(buffer);
+                        
 
             Console.ReadLine();
-        }
-
-        private static void ProcessBuffer(IBuffer<double> buffer)
-        {
-            double sum = 0.0;
-
-            Console.WriteLine("Buffer: ");
-
-            while (!buffer.IsEmpty)
-            {
-                sum += buffer.Read();
-            }
-
-            Console.WriteLine(sum);
-        }
-
-        private static void ProcessUserInput(IBuffer<double> buffer)
-        {
-            while (true)
-            {
-                double value = 0.0;
-
-                string input = Console.ReadLine();
-
-                if (double.TryParse(input, out value))
-                {
-                    buffer.Write(value);
-                    continue;
-                }
-
-                break;
-            }
         }
     }
 }
