646cf584e30b74d98c3b11a8566e4934c520e029
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-01-31 22:31:13 -0800

    Added extension methods to the IBuffer to demonstrate how to use genericly extend the buffer behavior

---
+++ CSharpGenerics/BufferExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CSharpGenerics
+{
+    public static class BufferExtensions
+    {
+        public delegate void Write<T>(T data);
+
+        public static IEnumerable<TOutput> AsEnumerableOf<T, TOutput>(this IBuffer<T> buffer)
+        {
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+
+            foreach (var item in buffer)
+            {
+//                if (!converter.CanConvertTo(typeof(TOutput))) continue;
+
+                TOutput result = (TOutput)converter.ConvertTo(item, typeof(TOutput));
+
+                yield return result;
+            }
+        }
+
+        public static void DumpContents<T>(this IBuffer<T> buffer, Write<T> write)
+        {
+            foreach (T item in buffer)
+            {
+                write(item);
+            }
+        }
+    }
+}
---
+++ CSharpGenericsTests/BufferExtensionsTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CSharpGenerics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharpGenericsTests
+{
+    public class BufferExtensionsTests
+    {
+        private ITestOutputHelper _output;
+
+        public BufferExtensionsTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void GivenABuffer_WithThreeDoublesAdded_WhenAsEnumerableOfIntIsCalled_ThenAnIEnumerableOfIntsIsReturned()
+        {
+            //arrange
+            Buffer<double> doubleBuffer = new Buffer<double>();
+            doubleBuffer.Write(1.1);
+            doubleBuffer.Write(2.2);
+            doubleBuffer.Write(3.3);
+
+            //act
+            IEnumerable<int> enumerableOfInts = doubleBuffer.AsEnumerableOf<double, int>();
+
+            //assert
+            List<int> listOfInts = enumerableOfInts.ToList();
+            listOfInts.Should().HaveCount(3);
+            listOfInts[0].Should().Be(1);
+            listOfInts[1].Should().Be(2);
+            listOfInts[2].Should().Be(3);
+        }
+
+        [Fact]
+        public void GivenABuffer_WithThreeDoublesAdded_WhenDumpContentsIsCalled_ThenTheBufferContentsAreOutputToTheConsole()
+        {
+            //arrange
+            Buffer<double> doubleBuffer = new Buffer<double>();
+            doubleBuffer.Write(1.1);
+            doubleBuffer.Write(2.2);
+            doubleBuffer.Write(3.3);
+
+            //act
+            doubleBuffer.DumpContents(UnitTestWrite);
+
+            //TextWriter standardOut = Console.Out;
+
+            //using (TextWriter tempOut = new StringWriter())
+            //{
+            //    Console.SetOut(tempOut);
+
+                //act
+
+
+            //    doubleBuffer.DumpContents(write);
+
+                //assert
+            //    string output = tempOut.ToString();
+            //    output.Should().Be("1.1\r\n2.2\r\n3.3\r\n");
+            //}
+
+            //cleanup
+            //Console.SetOut(standardOut);
+        }
+
+        public void UnitTestWrite(double data)
+        {
+            _output.WriteLine(data.ToString());
+        }
+
+    }
+}
